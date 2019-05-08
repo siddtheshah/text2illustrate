cf32dc20689d5bf24c3b6befba6155bb0ab64d6b
Sid <siddshah95@gmail.com>
2019-05-07 20:16:37 -0400

    Modified timeout and maven deps

--- stanfordcorenlpWrapper/corenlp.py
+++ stanfordcorenlpWrapper/corenlp.py
@@ -22,7 +22,7 @@ import requests
 
 
 class StanfordCoreNLP:
-    def __init__(self, path_or_host, port=None, memory='4g', lang='en', timeout=1500, quiet=True,
+    def __init__(self, path_or_host, port=None, memory='4g', lang='en', timeout=60, quiet=True,
                  logging_level=logging.WARNING):
         self.path_or_host = path_or_host
         self.port = port
